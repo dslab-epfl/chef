4c8741b78bb0281e08fc58ad80b4ab9cae1d1299
Stefan Bucur <stefan.bucur@epfl.ch>
2014-11-30 19:14:01 +0100

    s2e.h: Deprecating the old custom opcode in favor of the NOP instruction.

--- guest/include/bits/s2e-x86.h
+++ guest/include/bits/s2e-x86.h
@@ -33,10 +33,30 @@
  * All contributors are listed in the S2E-AUTHORS file.
  */
 
+#ifdef S2E_DISPATCH_CUSTOM
+/* (Deprecated) We define our own custom instruction, using a previously
+ * unallocated opcode sequence. Therefore, this construct would cause a
+ * processor exception on real hardware or non-S2E virtual environments.
+ */
 #define S2E_INSTRUCTION_COMPLEX(val1, val2)             \
     ".byte 0x0F, 0x3F\n"                                \
     ".byte 0x00, 0x" #val1 ", 0x" #val2 ", 0x00\n"      \
     ".byte 0x00, 0x00, 0x00, 0x00\n"
+
+#else
+/* We overload a multi-byte NOP instruction.  We reuse the 8-byte form
+ * NOP DWORD ptr [EAX + EAX*1 + 00000000H], corresponding to the sequence
+ * 0F 1F 84 00 00 00 00 00H
+ *
+ * The last five bytes can be changed arbitrarily, and we use them as follows:
+ *   Byte 3 - The S2E magic number 0x42
+ *   Byte 4-7 - A 32-bit S2E instruction payload.
+ */
+#define S2E_INSTRUCTION_COMPLEX(val1, val2)             \
+    ".byte 0x0F, 0x1F\n"                                \
+    ".byte 0x84, 0x42\n"                                \
+    ".byte 0x00, 0x" #val1 ", 0x" #val2 ", 0x00\n"
+#endif /* defined(S2E_DISPATCH_CUSTOM) */
 
 #define S2E_INSTRUCTION_SIMPLE(val)                     \
     S2E_INSTRUCTION_COMPLEX(val, 00)
