2d72f2e10297a795dfd7f87bb67b84e388f6d708
ayekat <takeya@bluewin.ch>
2015-05-20 17:05:40 +0200

    batch.py: Allow insertion of command "prefix"
    This allows the insertion of the docker/qemu command line we are later
    going to need.

--- ccli/parallel/batch.py
+++ ccli/parallel/batch.py
@@ -46,7 +46,7 @@ class Batch:
                 flattened += [':::'] + v
             return flattened
 
-        def get_cmd_line(self, path_results: str, variables: dict):
+        def get_cmd_line(self, prefix: [str], path_results: str, variables: dict):
             if not os.path.isdir(path_results):
                 os.mkdir(path_results)
 
@@ -57,7 +57,7 @@ class Batch:
                         '--joblog', '%s/log' % path_results]
             line = self.line
             args = self.flatten(self.resolve(self.filter(variables)))
-            return parallel + line + args
+            return parallel + prefix + line + args
 
 
     def __init__(self, path: str):
@@ -71,23 +71,21 @@ class Batch:
         for ctoken in yaml.tree['commands']:
             self.commands.append(Batch.Command(ctoken))
 
-    def get_cmd_lines(self, path_results: str):
+    def get_cmd_lines(self, prefix: [str], path_results: str):
         cmd_lines = []
-        i = 1
-        for c in self.commands:
-            cl = c.get_cmd_line('%s/exp%04d' % (path_results,i), self.variables)
+        for c, i in zip(self.commands, range(len(self.commands))):
+            cl = c.get_cmd_line(prefix, '%s/exp%04d' % (path_results, i), self.variables)
             cmd_lines.append(cl)
-            i = i + 1
         return cmd_lines
 
-    def run(self, path_results: str):
+    def run(self, prefix: [str], path_results: str):
         if os.path.isdir(path_results):
             print("%s: directory already exists" % path_results, file=sys.stderr)
             exit(1)
         else:
             os.mkdir(path_results)
 
-        cmd_lines = self.get_cmd_lines(path_results)
+        cmd_lines = self.get_cmd_lines(prefix, path_results)
         for c in cmd_lines:
             with open(os.devnull, 'w') as fp:
                 subprocess.call(c, stderr=fp, stdout=fp)
@@ -95,14 +93,14 @@ class Batch:
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: %s YAML [OUTDIR]" % sys.argv[0])
+        print("Usage: %s YAML [OUTDIR]" % sys.argv[0], file=sys.stderr)
         exit(1)
 
     path_yaml = sys.argv[1]
     path_results = PATH_RESULTS if len(sys.argv) < 3 else sys.argv[2]
 
     b = Batch(path_yaml)
-    b.run(path_results)
+    b.run([], path_results)
     print("The results have been logged in %s" % path_results)
 
 
