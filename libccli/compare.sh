459b358244043896a597b96ade556ff3af7d1eee
ayekat <takeya@bluewin.ch>
2015-06-09 11:21:43 +0200

    ccli: Make `compare` subcommand useful
    Simply compares whether the result of the first N query evaluation are
    the same (sat/unsat).

--- libccli/compare.sh
+++ libccli/compare.sh
@@ -1,38 +1,81 @@
 #!/usr/bin/env sh
+#
+# This script uses CVC3 to check the integrity of query results
+
+RUNNAME="$(basename "$0")"
+if [ -z "$INVOKENAME" ]; then
+	INVOKENAME="$RUNNAME"
+fi
+
+# COMPARE ======================================================================
 
 compare()
 {
+	compare_wrong=0
+	compare_array=''
 	printf "%4s | %5s | %5s\n" 'q' 'norm' 'comp'
 	echo '-----+-------+------'
-	for i in {1..12}; do
+	for i in $(seq $LIMIT); do
 		smtd="$(./third/cvc3/bin/cvc3 -lang smt2 data/smtlibdump/$( printf "%04d" $i).smt)"
 		cmpd="$(./third/cvc3/bin/cvc3 -lang smt2 data/compactdump/$(printf "%04d" $i).smt)"
 		if [ "$smtd" != "$cmpd" ]; then
+			compare_wrong=$(($compare_wrong + 1))
+			compare_array="$compare_array $i"
 			printf "\033[31m"
 		fi
 		printf "%4d | %5s | %5s\033[0m\n" $i "$smtd" "$cmpd"
 	done 2>/dev/null
+	echo '-----+-------+------'
+	if [ $compare_wrong -eq 0 ]; then
+		printf "\033[32mAll queries passed correctly\033[0m\n"
+	else
+		printf "\033[31m%d queries failed:%s\033[0m\n" \
+			$compare_wrong "$compare_array"
+	fi
+}
+
+# UTILITIES ====================================================================
+
+die()
+{
+	die_retval=$1
+	die_format="$2"
+	shift 2
+	printf "$die_format\n" "$@" >&2
+	exit $die_retval
 }
 
 # MAIN =========================================================================
 
-get_options()
+usage()
 {
-	LIMIT=0
-	while getopts hl: opt; do
-		case "$opt" in
-			h) help; exit 1 ;;
-			l) LIMIT="$OPTARG" ;;
-			'?') die_help ;;
-		esac
-	done
-	ARGSHIFT=$(($OPTIND - 1))
+	cat <<- EOF
+	Usage: $INVOKENAME LIMIT
+	EOF
+}
+
+get_limit()
+{
+	LIMIT="$1"
+	if [ -z "$LIMIT" ]; then
+		usage >&2
+		exit 1
+	fi
+	case "$LIMIT" in
+		''|*[!0-9]*) die 1 'Limit needs to be a numeric value' ;;
+		*) ;;
+	esac
+	if [ $LIMIT -le 0 ]; then
+		die 1 'Limit must be larger than zero'
+	fi
+	ARGSHIFT=1
 }
 
 main()
 {
-	get_options "$@"
+	get_limit "$@"
 	shift $ARGSHIFT
+	test $# -eq 0 || die 1 'Unknown argument: %s' "$1"
 	compare
 }
 
