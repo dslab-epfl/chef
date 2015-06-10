1bb396cb3989b1fbbb1e1b41fb0f8fb64b59daa4
ayekat <takeya@bluewin.ch>
2015-06-10 11:13:26 +0200

    ccli: Make `compare` more useful, add utils shell script
    Utils is a collection of functions that are often reused among all the
    shell scripts in ccli. Currently, only `compare` makes use of it.
    
    `compare` now has a proper interface, and allows choosing the solver
    (currently Z3 and CVC3 are supported). It has already found bugs.

--- libccli/compare.sh
+++ libccli/compare.sh
@@ -2,22 +2,43 @@
 #
 # This script uses CVC3 to check the integrity of query results
 
-RUNNAME="$(basename "$0")"
-if [ -z "$INVOKENAME" ]; then
-	INVOKENAME="$RUNNAME"
-fi
+. "$(readlink -f "$(dirname "$0")")/utils.sh"
 
 # COMPARE ======================================================================
 
 compare()
 {
+	case "$SOLVER" in
+	z3)
+		SOLVER_BIN="$($WHICH $SOLVER)"
+		SOLVER_ARGS='-smt2'
+		;;
+	cvc3)
+		SOLVER_BIN="$(find "$SRCPATH_ROOT" \
+		              -name 'cvc3' \
+		              -executable \
+		              -type f)"
+		SOLVER_ARGS='-lang smt2'
+		;;
+	*)
+		die_internal 'compare(): invalid solver %s' "$SOLVER"
+		;;
+	esac
+
 	compare_wrong=0
 	compare_array=''
-	printf "%4s | %5s | %5s\n" 'q' 'norm' 'comp'
+
+	printf "%4s | %5s | %5s\n" 'q' 'orig' 'comp'
 	echo '-----+-------+------'
-	for i in $(seq $LIMIT); do
-		smtd="$(./third/cvc3/bin/cvc3 -lang smt2 data/smtlibdump/$( printf "%04d" $i).smt)"
-		cmpd="$(./third/cvc3/bin/cvc3 -lang smt2 data/compactdump/$(printf "%04d" $i).smt)"
+	for i in $(seq $RANGE_OFFSET $(($RANGE_OFFSET + $RANGE_LENGTH - 1))); do
+		smtd="$("$SOLVER_BIN" \
+		        $SOLVER_ARGS \
+		        "$SRCPATH_ROOT/data/smtlibdump/$( printf "%04d" $i).smt")" \
+		        || smtd='error'
+		cmpd="$("$SOLVER_BIN" \
+		        $SOLVER_ARGS \
+		        "$SRCPATH_ROOT/data/compactdump/$(printf "%04d" $i).smt")" \
+		        || cmpd='wrong'
 		if [ "$smtd" != "$cmpd" ]; then
 			compare_wrong=$(($compare_wrong + 1))
 			compare_array="$compare_array $i"
@@ -27,22 +48,50 @@ compare()
 	done 2>/dev/null
 	echo '-----+-------+------'
 	if [ $compare_wrong -eq 0 ]; then
-		printf "\033[32mAll queries passed correctly\033[0m\n"
+		success "queries passed (%d)\n" $RANGE_LENGTH
 	else
-		printf "\033[31m%d queries failed:%s\033[0m\n" \
-			$compare_wrong "$compare_array"
+		fail "queries failed (%d):%s\n" $compare_wrong "$compare_array"
+		if [ $compare_wrong -eq 1 ]; then
+			success "original:\n"
+			"$SOLVER_BIN" \
+				$SOLVER_ARGS \
+				"$SRCPATH_ROOT/data/smtlibdump/$( printf "%04d" $i).smt"
+			fail "compact:\n"
+			"$SOLVER_BIN" \
+				$SOLVER_ARGS \
+				"$SRCPATH_ROOT/data/compactdump/$( printf "%04d" $i).smt"
+		fi
 	fi
 }
 
-# UTILITIES ====================================================================
+# DRYRUN =======================================================================
 
-die()
+dryrun()
 {
-	die_retval=$1
-	die_format="$2"
-	shift 2
-	printf "$die_format\n" "$@" >&2
-	exit $die_retval
+	util_dryrun
+	cat <<- EOF
+	RANGE_OFFSET=$RANGE_OFFSET
+	RANGE_LENGTH=$RANGE_LENGTH
+	SOLVER=$SOLVER
+	DIRECT=$(as_boolean $DIRECT)
+	EOF
+}
+
+# DOCKER =======================================================================
+
+docker_compare()
+{
+	docker run \
+		-t \
+		-i \
+		--rm \
+		-v "$SRCPATH_ROOT":"$DOCKER_HOSTPATH" \
+		dslab/s2e-chef:v0.6 \
+		"$DOCKER_HOSTPATH/$RUNDIR/$RUNNAME" \
+			-z \
+			"$SOLVER" \
+			$RANGE_OFFSET \
+			$RANGE_LENGTH
 }
 
 # MAIN =========================================================================
@@ -50,33 +99,100 @@ die()
 usage()
 {
 	cat <<- EOF
-	Usage: $INVOKENAME LIMIT
+	Usage: $INVOKENAME [OPTIONS ...] SOLVER OFFSET [LENGTH]
 	EOF
 }
 
-get_limit()
+help()
 {
-	LIMIT="$1"
-	if [ -z "$LIMIT" ]; then
-		usage >&2
-		exit 1
-	fi
-	case "$LIMIT" in
-		''|*[!0-9]*) die 1 'Limit needs to be a numeric value' ;;
-		*) ;;
+	usage
+
+	cat <<- EOF
+
+	Solvers:
+	  z3
+	  cvc3
+	  stp        (not yet supported)
+
+	Offset, Length:
+	  Strictly positive values denoting the first query to start with, and the
+	  number of queries to compare. The length is 1 by default.
+
+	Options:
+	  -h         Display this help
+	  -y         Dry run: print variables and exit
+	  -z         Direct mode (don't use docker)
+	EOF
+}
+
+get_options()
+{
+	DIRECT=$FALSE
+	DRYRUN=$FALSE
+
+	while getopts hyz opt; do
+		case "$opt" in
+			h) help; exit 1 ;;
+			y) DRYRUN=$TRUE ;;
+			z) DIRECT=$TRUE ;;
+			'?') die_help ;;
+		esac
+	done
+	ARGSHIFT=$(($OPTIND - 1))
+}
+
+get_solver()
+{
+	SOLVER="$1"
+	test -n "$SOLVER" || die_help 'Missing solver'
+	case "$SOLVER" in
+		z3|cvc3) ;;
+		stp) die 1 'This solver is not supported yet' ;;
+		*) die_help 'Unknown solver: %s' "$SOLVER" ;;
 	esac
-	if [ $LIMIT -le 0 ]; then
-		die 1 'Limit must be larger than zero'
-	fi
 	ARGSHIFT=1
+}
+
+get_range_offset()
+{
+	RANGE_OFFSET="$1"
+	test -n "$RANGE_OFFSET" || die_help 'Missing offset'
+	is_numeric "$RANGE_OFFSET" || die 1 'Offset needs to be a numeric value'
+	test $RANGE_OFFSET -gt 0 || die 1 'Offset must be larger than zero'
+	ARGSHIFT=1
+}
+
+get_range_length()
+{
+	RANGE_LENGTH="$1"
+	if [ -z "$RANGE_LENGTH" ]; then
+		RANGE_LENGTH=1
+		ARGSHIFT=0
+	else
+		is_numeric "$RANGE_LENGTH" || die 1 'Length needs to be a numeric value'
+		test $RANGE_LENGTH -gt 0 || die 1 'Length must be larger than zero'
+		ARGSHIFT=1
+	fi
 }
 
 main()
 {
-	get_limit "$@"
-	shift $ARGSHIFT
-	test $# -eq 0 || die 1 'Unknown argument: %s' "$1"
-	compare
+	for getargs in get_options get_solver get_range_offset get_range_length; do
+		$getargs "$@"
+		shift $ARGSHIFT
+	done
+	test $# -eq 0 || die_help "trailing arguments: @"
+
+	if [ $DRYRUN -eq $TRUE ]; then
+		dryrun
+		exit
+	fi
+
+	if [ $DIRECT -eq $TRUE ]; then
+		compare
+	else
+		docker_compare
+	fi
 }
 
 set -e
---
+++ libccli/utils.sh
@@ -0,0 +1,220 @@
+# Shell script utilities shared by most scripts
+#
+# Maintainer: Tinu Weber <martin.weber@epfl.ch>
+
+# VARIABLES ====================================================================
+
+RUNNAME="$(basename "$0")"
+RUNPATH="$(readlink -f "$(dirname "$0")")"
+RUNDIR="$(basename "$RUNPATH")"
+SRCPATH_ROOT="$(dirname "$RUNPATH")"
+SRCDIR_ROOT="$(basename "$SCRPATH_ROOT")"
+
+if [ -z "$INVOKENAME" ]; then
+	INVOKENAME="$RUNNAME"
+fi
+
+DOCKER_IMAGE='dslab/s2e-chef:v0.6'
+DOCKER_HOSTPATH='/host'
+
+# MESSAGES =====================================================================
+
+FATAL_="[\033[31mFATAL\033[0m]"
+FAIL_="[\033[31mFAIL\033[0m]"
+WARN_="[\033[33mWARN\033[0m]"
+_OK__="[\033[32m OK \033[0m]"
+SKIP_="[\033[34mSKIP\033[0m]"
+
+note()
+{
+	note_format="$1"
+	shift
+	printf "       $note_format\n" "$@"
+}
+
+warn()
+{
+	warn_format="$1"
+	shift
+	printf "$WARN_ $warn_format\n" "$@" >&2
+}
+
+skip()
+{
+	skip_format="$1"
+	shift
+	printf "$SKIP_ $skip_format\n" "$@"
+}
+
+emphasised()
+{
+	important_colour=$1
+	important_format="$2"
+	shift 2
+	printf "\033[1;${important_colour}m>>>\033[0m $important_format" "$@"
+}
+
+success()
+{
+	success_format="$1"
+	shift
+	emphasised 32 "$success_format" "$@"
+}
+
+fail()
+{
+	fail_format="$1"
+	shift
+	emphasised 31 "$fail_format" "$@"
+}
+
+track()
+{
+	track_msg="$1"
+	shift
+
+	if [ $VERBOSE -eq $TRUE ]; then
+		if "$@"; then
+			return $TRUE
+		else
+			return $FALSE
+		fi
+	else
+		if [ -z "$LOGFILE" ]; then
+			die_internal "track(): no log file specified"
+		fi
+		printf "\033[s[ .. ] %s" "$track_msg"
+		track_status=$_OK__
+		{ "$@" || track_status=$FAIL_; } >>"$LOGFILE" 2>>"$LOGFILE"
+		printf "\033[u%s %s\n" "$track_status" "$track_msg"
+		test $track_status -eq $_OK__ && return $TRUE || return $FALSE
+	fi
+}
+
+# USER INPUT ===================================================================
+
+ask()
+{
+	ask_colour=$1
+	ask_default="$2"
+	ask_format="$3"
+	shift 3
+	case "$ask_default" in
+		[Yy]*) ask_sel='[Y/n]';;
+		[Nn]*) ask_sel='[y/N]';;
+		*) die_internal "ask(): invalid default '%s'" "$ask_default" ;;
+	esac
+	while true; do
+		emphasised $ask_colour "$ask_format $ask_sel " "$@"
+		read a
+		test -n "$a" || a="$ask_default"
+		case "$a" in
+			[Yy]*) return 0;;
+			[Nn]*) return 1;;
+			*) ;;
+		esac
+	done
+}
+
+# EXIT =========================================================================
+
+# General die
+die()
+{
+	die_retval=$1
+	die_format="$2"
+	shift 2
+	printf "$die_format\n" "$@" >&2
+	exit $die_retval
+}
+
+# Wrong command line
+die_help()
+{
+	die_help_format="$1"
+	if [ -n "$die_help_format" ]; then
+		shift
+		printf "$die_help_format\n" "$@" >&2
+	fi
+	if is_function usage; then
+		usage >&2
+	fi
+	die 1 'Run `%s -h` for help.' "$INVOKENAME"
+}
+
+# Internal reasons to crash
+die_internal()
+{
+	die_internal_format="$1"
+	die 127 "internal error: $die_internal_format\n" "$@"
+}
+
+# SHELL ========================================================================
+
+# Yes, fuck logic
+TRUE=0
+FALSE=1
+
+# Test if a variable is purely (decimal) numeric
+is_numeric()
+{
+	case "$1" in
+		''|*[!0-9]*) return $FALSE ;;
+		*) return $TRUE ;;
+	esac
+}
+
+# Test if a function is defined
+is_function()
+{
+	if type "$1" >/dev/null 2>&1; then
+		return $TRUE
+	else
+		return $FALSE
+	fi
+}
+
+# Give human readable representation for boolean
+as_boolean()
+{
+	if [ $1 -eq $TRUE ]; then
+		echo 'true'
+	else
+		echo 'false'
+	fi
+}
+
+# COMMANDS =====================================================================
+
+# Search for coreutil's `which`
+IFS_="$IFS"
+IFS=:
+WHICH='which'
+for p in "$PATH"; do
+	if [ -x "$p/which" ]; then
+		WHICH="$p/which"
+		break
+	fi
+done
+IFS="$IFS_"
+
+# DEBUG ========================================================================
+
+util_dryrun()
+{
+	cat <<- EOF
+	RUNNAME=$RUNNAME
+	RUNPATH=$RUNPATH
+	RUNDIR=$RUNDIR
+	SRCPATH_ROOT=$SRCPATH_ROOT
+	SRCDIR_ROOT=$SRCDIR_ROOT
+	INVOKENAME=$INVOKENAME
+	DOCKER_IMAGE=$DOCKER_IMAGE
+	DOCKER_HOSTPATH=$DOCKER_HOSTPATH
+	FATAL_=$FATAL_
+	FAIL_=$FAIL_
+	WARN_=$WARN_
+	_OK__=$_OK__
+	SKIP_=$SKIP_
+	EOF
+}
