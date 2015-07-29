5eb942c413fb03684650809251564c296af80b00
Stefan Bucur <stefan.bucur@epfl.ch>
2015-07-29 14:56:51 +0200

    Utils: New hex string printer.

--- qemu/s2e/Utils.h
+++ qemu/s2e/Utils.h
@@ -42,6 +42,7 @@
 #include <iomanip>
 #include <sstream>
 #include <deque>
+#include <string>
 #include <inttypes.h>
 #include <llvm/Support/raw_ostream.h>
 #include <klee/Expr.h>
@@ -57,11 +58,24 @@ struct hexval {
     hexval(void* _value, int _width=0): value((uint64_t)_value), width(_width) {}
 };
 
+struct hexstring {
+	const std::string &value;
+
+	hexstring(const std::string &_value) : value(_value) {}
+};
+
 inline llvm::raw_ostream& operator<<(llvm::raw_ostream& out, const hexval& h)
 {
     out << "0x";
     out.write_hex(h.value);
     return out;
+}
+
+inline llvm::raw_ostream& operator<<(llvm::raw_ostream& out, const hexstring &s) {
+	out << '"';
+	out.write_escaped(s.value, true);
+	out << '"';
+	return out;
 }
 
 inline std::ostream& operator<<(std::ostream& out, const hexval& h)
