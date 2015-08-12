7c4f26aa812dc562e1969193ca45d81f4e6dd51d
Tinu Weber <martin.weber@epfl.ch>
2015-08-12 17:11:05 +0200

    docs: Recompile documentation before publishing

--- docs/publish.sh
+++ docs/publish.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 set -e
 
-# if git is not initialised, clone first:
+# Make sure the git repository is there:
 if [ ! -d build/html/.git ]; then
 	echo 'could not find git repository in build/html, cloning'
 	rm -rf build/html/
 	git clone -b gh-pages git@github.com:dslab-epfl/chef build/html
-	make html
 fi
 
+# Make sure the compilation is up to date:
+make html
+
+# Commit and push:
 cd build/html
 numchange="$(git status --porcelain | wc -l)"
 if [ "$numchange" != '0' ]; then
