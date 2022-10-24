9e99c0eb7ef477f7a37ba495807173d45fa7ca44
Petr Šťastný <petr@soptik.tech>
2022-10-24 19:24:00 +0800

    Specified copied dotfiles

--- ctltools/build.sh
+++ ctltools/build.sh
@@ -40,7 +40,10 @@ z3_extract()
 z3_configure()
 {
     mv "z3-z3-4.11.2"/* . || return $FAILURE
-    mv "z3-z3-4.11.2"/.* . || return $FAILURE
+    mv "z3-z3-4.11.2"/.dockerignore . || return $FAILURE
+    mv "z3-z3-4.11.2"/.gitattributes . || return $FAILURE
+    mv "z3-z3-4.11.2"/.github . || return $FAILURE
+    mv "z3-z3-4.11.2"/.gitignore . || return $FAILURE
     rm -d "z3-z3-4.11.2" || return "$FAILURE"
 	python2 scripts/mk_make.py || return $FAILURE
 }
